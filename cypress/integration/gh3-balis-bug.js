0d006ae5351d63e8964ceaf33739c621c214a388
Matt Fletcher <mattflet@gmail.com>
2021-12-12 20:27:40 +0000

    #3 - Cypress test screenshot

--- cypress/integration/gh3-balis-bug.js
+++ cypress/integration/gh3-balis-bug.js
@@ -4,8 +4,8 @@ describe('GitHub issue 3 yellow square bug', () => {
     cy.get('#form-input').type('bingo{enter}');
     cy.get('#form-input').type('basic{enter}');
     cy.get('#form-input').type('balis{enter}');
+    cy.screenshot('gh-3-balis-not-win');
     cy.contains('won').should('not.exist');
     cy.get('.latest-guess').should('have.text', 'balis');
-    cy.screenshot('gh-3-balis-not-win');
   });
 });
