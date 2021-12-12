89183b07a73daff26ebf2152627757b4a5628fb0
Matt Fletcher <mattflet@gmail.com>
2021-12-12 12:57:53 +0000

    #7 - Lint

--- src/functions/convertToObject.ts
+++ src/functions/convertToObject.ts
@@ -1,7 +1,7 @@
 function convertToObject(word: string) {
   return word.split('').map((char) => {
     if (char === '_') {
-      return { char: null }
+      return { char: null };
     }
     return { char };
   });
