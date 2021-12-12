529437c742ac4ed20699d23a69decfb920b8d71e
Matt Fletcher <mattflet@gmail.com>
2021-12-12 13:07:34 +0000

    #3 - Added cypress test that fails for bug shown in issue

---
+++ cypress/integration/gh3-balis-bug.js
@@ -0,0 +1,11 @@
+describe('GitHub issue 3 yellow square bug', () => {
+  it('does not win', () => {
+    cy.visit('/#basis');
+    cy.get('#form-input').type('bingo{enter}');
+    cy.get('#form-input').type('basic{enter}');
+    cy.get('#form-input').type('balis{enter}');
+    cy.contains('won').should('not.exist');
+    cy.get('.latest-guess').should('have.text', 'balis');
+    cy.screenshot('gh-3-balis-not-win');
+  });
+});
